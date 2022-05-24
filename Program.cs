f290d5fcb6f4b0104eff5c399368272f11278480
Dhruv Rayat <dhruvrayat50@gmail.com>
2022-05-24 21:11:41 +1200

    GitHub Files Added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TimeTable
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world!");
+        }
+    }
+}
